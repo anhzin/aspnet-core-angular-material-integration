0c0bb0e6e4e23daf2cada747ab2bf9e55abe459c
Jaime Still <jpstill85@gmail.com>
2017-07-17 15:20:50 -0400

    update home component

--- ClientApp/app/components/home/home.component.ts
+++ ClientApp/app/components/home/home.component.ts
@@ -6,4 +6,36 @@ import { Component } from '@angular/core';
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+    cards = [
+        {
+            src: '/assets/github-icon.svg',
+            url: 'https://github.com/JaimeStill/AspNetCoreMaterial',
+            header: 'See the full project on GitHub'
+        },
+        {
+            src: '/assets/vs-code.svg',
+            url: 'https://code.visualstudio.com',
+            header: 'Download Visual Studio Code'
+        },
+        {
+            src: '/assets/nodejs.svg',
+            url: 'https://nodejs.org',
+            header: 'Download Node.js'
+        },
+        {
+            src: '/assets/microsoft.svg',
+            url: 'https://github.com/aspnet/JavaScriptServices',
+            header: 'ASP.NET Core JavaScriptServices'
+        },
+        {
+            src: '/assets/angular-icon.svg',
+            url: 'https://angular.io',
+            header: 'Angular'
+        },
+        {
+            src: '/assets/angular-icon.svg',
+            url: 'https://material.angular.io',
+            header: 'Angular Material'
+        }
+    ]
 }
