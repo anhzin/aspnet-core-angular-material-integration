eb696a94798d060bac3fd2d457efbe5bed783ecf
Jaime Still <jpstill85@gmail.com>
2017-07-20 11:13:04 -0400

    toolbar theme menu

--- ClientApp/app/components/app/app.component.ts
+++ ClientApp/app/components/app/app.component.ts
@@ -10,11 +10,16 @@ import {MdIconRegistry} from '@angular/material';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-    materialTheme = 'green-app';
+    materialTheme: Theme;
 
     themes: Theme[] = new Array<Theme>();
 
     constructor(private themeService: ThemeService, iconRegistry: MdIconRegistry, sanitizer: DomSanitizer) {
+        this.materialTheme = {
+            id: 'green-app',
+            display: 'Green'
+        };
+
         iconRegistry.addSvgIcon(
         'github',
         sanitizer.bypassSecurityTrustResourceUrl('/assets/github-icon.svg'));
@@ -24,11 +29,15 @@ export class AppComponent implements OnInit {
         this.themes = this.themeService.getThemes();
 
         this.themeService.activeTheme.subscribe(theme => {
-            this.materialTheme = theme.id;
+            this.materialTheme = theme;
         });
     }
 
     onActivate(e, mainContainer) {
         document.querySelector('div.mat-sidenav-content').scrollTop = 0;
+    }
+
+    setTheme(theme: Theme) {
+        this.themeService.setTheme(theme);
     }
 }
